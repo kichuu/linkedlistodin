534489649ee49bae41e51018915de6b066c02d30
krishnadev <krishnadevkichus@gmail.com>
2024-10-04 21:17:14 +0530

    created classes for node and ll

---
+++ index.js
@@ -0,0 +1,14 @@
+class Node {
+    constructor(value = null, nextNode = null) {
+      this.value = value;     
+      this.nextNode = nextNode;
+    }
+  }
+  
+  class LinkedList {
+    constructor() {
+      this.headNode = null; 
+      this.length = 0;     
+    }
+  }
+  
